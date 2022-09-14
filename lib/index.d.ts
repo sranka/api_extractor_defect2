d3fd3c7ef7f3b16866609862b834dd8b6f6cc8ba
Pavel Zavora <pavel.zavora@bonitoo.io>
2022-09-14 09:50:18 +0200

    add STRING_VALUE as another example

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -1,9 +1,11 @@
 /** Property that offers a function that returns flux-sanitized value of an object.  */
 export declare const FLUX_VALUE: unique symbol;
+export declare const STRING_VALUE = "ADDED_just_to_know_if_it_is_scoped-to_symbol_only";
 /**
  * A flux parameter can print its (sanitized) flux value.
  */
 export interface FluxParameterLike {
     [FLUX_VALUE](): string;
+    [STRING_VALUE](): string;
 }
 //# sourceMappingURL=index.d.ts.map
--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,11 @@
 /** Property that offers a function that returns flux-sanitized value of an object.  */
-export const FLUX_VALUE = Symbol('FLUX_VALUE')
+export const FLUX_VALUE = Symbol("FLUX_VALUE");
+export const STRING_VALUE = "ADDED_just_to_know_if_it_is_scoped-to_symbol_only";
 
 /**
  * A flux parameter can print its (sanitized) flux value.
  */
 export interface FluxParameterLike {
-  [FLUX_VALUE](): string
+  [FLUX_VALUE](): string;
+  [STRING_VALUE](): string;
 }
